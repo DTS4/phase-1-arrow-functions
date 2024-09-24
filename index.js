874c7e28b704a14dfc1b4103935db281a20283de
Keith <keith.githinji@student.moringaschool.com>
2024-09-24 19:47:16 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (parameter1, parameter2) => {
+    console.log(`divide ${parameter1}`);
+    console.log(`divide ${parameter2}`);
+    return parameter1 / parameter2;
+}
+divide(2000/100)
+
+const square = (parameter) => {
+    return parameter * parameter;
+}
+
+const add = (parameter1, parameter2) => {
+    console.log(`add ${parameter1}`);
+    console.log(`add ${parameter2}`);
+    return parameter1 + parameter2;
+  }
+  add(1,2);
